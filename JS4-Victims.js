6d408eaa70428cf1cc4b7abfbf6adb346048ba5c
Student <kitty8mars@gmail.com>
2015-06-23 17:06:19 -0600

    Good day today

---
+++ JS4-Victims.js
@@ -0,0 +1,20 @@
+// Example from previous project
+// var postalCode = prompt("Please enter your 5-digit postal code");
+// 	console.log(postalCode);
+// 	alert(postalCode.length === 5)
+
+var victimName = [];
+var victimPhone = [];
+var victimStreet = [];
+
+var volunteerName = [];
+var volunteerPhone = [];
+var volunteerStreet = [];
+
+var numberOfVictims = prompt("Please enter the number numberOfVictims");
+	console.log(numberOfVictims);
+
+// var victimInfo = 
+// [name, ph]
+
+// prompt
