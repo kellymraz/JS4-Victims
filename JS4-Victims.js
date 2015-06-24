9896681914b5a4c1bb228940f300adb4256293ae
Student <kitty8mars@gmail.com>
2015-06-24 17:23:48 -0600

    Eeeeeeeeee

--- JS4-Victims.js
+++ JS4-Victims.js
@@ -7,14 +7,61 @@ var victimName = [];
 var victimPhone = [];
 var victimStreet = [];
 
+
+
+var numberOfVictims = prompt("Please enter the number of victims");
+	console.log(numberOfVictims);
+
+
+for (var i = 1; i <= numberOfVictims; i++) {
+	var name = prompt("Please enter the victim's name");
+	var phone = prompt("Please enter the victim's phone number");
+	var street = prompt("Please enter the victim's street name");
+	}
+
+
+	var victimInfo = {
+ 	name: name,
+ 	phone: phone,
+ 	address: street
+
+	};
+
+	console.log(victimInfo)
+
+// VOLUNTEER SECTION
+
+var numberOfVolunteers = prompt("Please enter the number of volunteers");
+	console.log(numberOfVolunteers);
+
 var volunteerName = [];
 var volunteerPhone = [];
 var volunteerStreet = [];
 
-var numberOfVictims = prompt("Please enter the number numberOfVictims");
-	console.log(numberOfVictims);
+for (var i = 1; i <= numberOfVolunteers; i++) {
+	var name = prompt("Please enter the volunteer's name");
+	var phone = prompt("Please enter the volunteer's phone number");
+	var street = prompt("Please enter the volunteer's street name");
+	}
 
-// var victimInfo = 
-// [name, ph]
+	var volunteerInfo = {
+		name: name,
+		phone: phone,
+		address: street
 
-// prompt
+	};
+
+	console.log(volunteerInfo)
+
+	alert("There are " + numberOfVictims + "  victim(s) and " + numberOfVolunteers + " volunteer(s)")
+
+
+
+
+
+
+
+
+
+
+
